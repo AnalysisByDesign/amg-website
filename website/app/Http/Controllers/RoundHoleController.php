ac0c0ee3232dd515a028e2e53a6bdfaab6e455fa
Dave Rix <dave@analysisbydesign.co.uk>
2019-12-03 23:37:41 +0000

    Add a bunch of model and controller placeholders

---
+++ website/app/Http/Controllers/RoundHoleController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\RoundHole;
+use Illuminate\Http\Request;
+
+class RoundHoleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\RoundHole  $roundHole
+     * @return \Illuminate\Http\Response
+     */
+    public function show(RoundHole $roundHole)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\RoundHole  $roundHole
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(RoundHole $roundHole)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\RoundHole  $roundHole
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, RoundHole $roundHole)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\RoundHole  $roundHole
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(RoundHole $roundHole)
+    {
+        //
+    }
+}
